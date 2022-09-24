ae6c77a9fad04de2c6e7c606c86f9c9dedc14e7f
nel <nellisolomon@yandex.ru>
2022-09-24 19:47:25 +0400

    решение задач

---
+++ 41/Program.cs
@@ -0,0 +1,61 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Сколько чисел необходимо ввести? ");
+bool isNumber = int.TryParse(Console.ReadLine(), out int len);
+
+if(!isNumber || len < 1)
+{
+    Console.WriteLine("Данные введены неверно");
+    return;
+}
+
+int[] arr = MakeArray(len);
+
+PrintArray(arr);
+Console.WriteLine($"Количество положительных чисел: {CountPositiveNumbers(arr)}");
+
+
+
+int[] MakeArray(int length) 
+{
+    int[] newArray = new int[length];
+
+    Console.WriteLine("Введите числа: ");
+
+    for (int i = 0; i < length; i++)
+    {
+        try
+        {
+            newArray[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        catch
+        {
+            Console.WriteLine("Введено НЕ число! Символ будет заменен на 0.");
+        }
+    }
+
+    return newArray;
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("Массив: [ ");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine($"{array[array.Length - 1]} ]");
+}
+
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
---
+++ 43/Program.cs
@@ -0,0 +1,57 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Введите k1: ");
+bool isNumK1 = double.TryParse(Console.ReadLine(), out double k1);
+Console.Write("Введите b1: ");
+bool isNumB1 = double.TryParse(Console.ReadLine(), out double b1);
+Console.Write("Введите k2: ");
+bool isNumK2 = double.TryParse(Console.ReadLine(), out double k2);
+Console.Write("Введите b2: ");
+bool isNumB2 = double.TryParse(Console.ReadLine(), out double b2);
+
+if(!isNumK1 || !isNumB1 || !isNumK2 || !isNumB2)
+{
+    Console.WriteLine("Данные введены неверно");
+    return;
+}
+
+if (CheckParallelism(k1, k2))
+{
+    Console.WriteLine("Прямые параллельны или совпадают");
+}
+else
+{
+    Console.Write($"Координаты точки пересечения: ");
+    PrintArray(FindCoordinates(k1, b1, k2, b2));
+}
+
+
+
+bool CheckParallelism(double k1, double k2)
+{
+    if (k1 == k2) return true;
+    return false;
+}
+
+
+double[] FindCoordinates(double k1, double b1, double k2, double b2)
+{
+    double x = (b2-b1) / (k1-k2);
+    double y = k1 * x + b1;
+    
+    double[] array = new double[2];
+    array[0] = x;
+    array[1] = y;
+    return array;
+}
+
+
+void PrintArray(double[] array)
+{
+    Console.Write("(");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine($"{array[array.Length - 1]})");
+}
